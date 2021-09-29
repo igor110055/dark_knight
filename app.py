98abcf15077034c0f152d4feeed23e067398757e
Terrence <takshingc@hotmail.com>
2021-09-29 23:55:34 +0800

    feat: add dynamic triangle

--- app.py
+++ app.py
@@ -12,17 +12,22 @@ last_update_ids = dict()
 best_prices = dict()
 
 
-async def main(symbols):
+async def main(natural, synthetic):
+    natural = natural.upper()
+    synthetic = {c.upper(): side for c, side in synthetic.items()}
+    symbols = [natural] + list(synthetic.keys())
     for symbol in symbols:
-        best_prices[symbol.upper()] = [0, 0, 0, 0]
+        best_prices[symbol.upper()] = dict(bid=[0, 0], ask=[0, 0])
 
     async with websockets.connect(
             "wss://stream.binance.com:9443/ws") as websocket:
+        params = [f"{symbol.lower()}@depth@100ms" for symbol in symbols]
+        print(params)
         payload = {
             "method": "SUBSCRIBE",
             # "params": ["btcusdt@aggTrade", "btcusdt@depth"],
             # 'params': ['btcusdt@depth@100ms'],
-            'params': [f"{symbol}@depth@100ms" for symbol in symbols],
+            'params': [f"{symbol.lower()}@depth@100ms" for symbol in symbols],
             "id": 1
         }
         await websocket.send(json.dumps(payload))
@@ -37,17 +42,17 @@ async def main(symbols):
 
             if symbol not in last_update_ids:
                 cached_responses.append(response)
-                get_order_book_snapshot(symbol)
+                get_order_book_snapshot(symbol, natural, synthetic)
                 continue
 
-            update_order_book(response)
+            update_order_book(response, natural, synthetic)
             # end_sequence = response['u']
             # start_sequence = response['U']
             # print(last_update_id)
             # print(response)
 
 
-def update_order_book(response):
+def update_order_book(response, natural, synthetic):
     end_sequence = response['u']
     symbol = response['s']
 
@@ -80,25 +85,63 @@ def update_order_book(response):
     best_bid_size = ob['bids'][best_bid_price]
     best_ask_price = min(ob['asks'])
     best_ask_size = ob['asks'][best_ask_price]
-    best_prices[symbol] = (best_bid_price, best_bid_size, best_ask_price,
-                           best_ask_size)
+    best_prices[symbol] = {
+        'bid': [best_bid_price, best_bid_size],
+        'ask': [best_ask_price, best_ask_size]
+    }
     # print(symbol, best_bid_price, best_bid_size, best_ask_price, best_ask_size)
 
-    sell_natural = best_prices['TROYUSDT'][0] or 1
-    buy_synthetic = best_prices['TROYBNB'][2] * best_prices['BNBUSDT'][2] or 1
+    # print(best_prices)
 
-    buy_natural = best_prices['TROYUSDT'][2] or 1
-    sell_synthetic = best_prices['TROYBNB'][0] * best_prices['BNBUSDT'][0] or 1
+    (left_curr, left_normal), (right_curr, right_normal) = synthetic.items()
 
-    # if sell_natural > buy_synthetic or sell_synthetic > buy_natural:
-    print(
-        sell_natural,
-        buy_synthetic,
-        (sell_natural - buy_synthetic) / buy_synthetic *
-        100,  # which one is the base
-        buy_natural,
-        sell_synthetic,
-        (sell_synthetic - buy_natural) / buy_natural * 100)
+    natural_bid = best_prices[natural]['bid'][0]
+
+    if left_normal:
+        left_synthetic_ask = best_prices[left_curr]['ask'][0]
+    else:
+        bid = best_prices[left_curr]['bid'][0]
+        if not bid:
+            return
+        left_synthetic_ask = 1 / best_prices[left_curr]['bid'][0]
+
+    if right_normal:
+        right_synthetic_ask = best_prices[right_curr]['ask'][0]
+    else:
+        bid = best_prices[right_curr]['bid'][0]
+        if not bid:
+            return
+        right_synthetic_ask = 1 / best_prices[right_curr]['bid'][0]
+
+    synthetic_ask = left_synthetic_ask * right_synthetic_ask or 1
+
+    natural_ask = best_prices[natural]['ask'][0] or 1
+
+    if left_normal:
+        left_synthetic_ask = best_prices[left_curr]['bid'][0]
+    else:
+        ask = best_prices[left_curr]['ask'][0]
+        if not ask:
+            return
+        left_synthetic_ask = 1 / ask
+
+    if right_normal:
+        right_synthetic_ask = best_prices[right_curr]['bid'][0]
+    else:
+        ask = best_prices[right_curr]['ask'][0]
+        if not ask:
+            return
+        right_synthetic_ask = 1 / ask
+
+    synthetic_bid = left_synthetic_ask * right_synthetic_ask or 1
+
+    if (diff := natural_bid - synthetic_ask) > 0:
+        print('Buy synthetic, sell natural', natural_bid, synthetic_ask,
+              diff / synthetic_ask * 100)
+
+    if (diff := synthetic_bid - natural_ask) > 0:
+        print('Buy natural, sell synthetic', synthetic_bid, natural_ask,
+              diff / natural_ask * 100)
 
     # pprint(
     #     sorted(order_book['bids'].items(),
@@ -109,7 +152,7 @@ def update_order_book(response):
     # check start_sequence increment
 
 
-def get_order_book_snapshot(symbol):
+def get_order_book_snapshot(symbol, natural, synthetic):
     global last_update_id
 
     response = requests.get(
@@ -124,9 +167,11 @@ def get_order_book_snapshot(symbol):
     order_book[symbol] = dict(bids=bids, asks=asks)
 
     for response in cached_responses:
-        update_order_book(response)
+        update_order_book(response, natural, synthetic)
 
 
 if __name__ == '__main__':
-    symbols = ['bnbusdt', 'troyusdt', 'troybnb']
-    asyncio.run(main(symbols))
+    # symbols = ['bnbusdt', 'troyusdt', 'troybnb']
+    natural = 'waxpusdt'
+    synthetic = {'waxpbnb': True, 'bnbusdt': True}
+    asyncio.run(main(natural, synthetic))
